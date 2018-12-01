52ed56761fc6db5bdff1a35c25c2f215fba47722
Klaas-Jan Stellema <k.j.stellema@gmail.com>
2018-12-01 19:42:18 +0100

    added first seen frequency

--- day1/day01.js
+++ day1/day01.js
@@ -1,7 +1,23 @@
 var fs = require('fs');
 
 fs.readFile('frequencies.txt', 'utf8', function(err, contents) {
+    const seen = new Set();
+    
     const frequencies = contents.replace(/\+/g, '').split('\n');
-    const sum = frequencies.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+    let found = false;
+    let sum = 0;
+    while(!found) {
+        sum = frequencies.reduce((a, b) => {
+            const result =  parseInt(a) + parseInt(b);
+            if (!seen.has(result)) {
+                seen.add(result);
+            } else {
+                console.log('already seen: ', result);
+                found = true;
+            }
+            return result;
+        }, sum);
+    }
+    
     console.log('total: ', sum);
 });
