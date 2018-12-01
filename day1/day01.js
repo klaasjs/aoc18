c3adeca7fb4a4ce8ef2564ac6fde2cc2afe5bbb7
Klaas-Jan Stellema <k.j.stellema@gmail.com>
2018-12-01 17:15:58 +0100

    fixed replace

--- day1/day01.js
+++ day1/day01.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 fs.readFile('frequencies.txt', 'utf8', function(err, contents) {
-    const frequencies = contents.replace(/\+/g, '+').split('\n');
+    const frequencies = contents.replace(/\+/g, '').split('\n');
     const sum = frequencies.reduce((a, b) => parseInt(a) + parseInt(b), 0);
     console.log('total: ', sum);
 });
