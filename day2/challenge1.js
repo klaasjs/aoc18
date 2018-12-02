330275ae6ee5b3e7a00182e1529093bd7598260b
Klaas-Jan Stellema <k.j.stellema@gmail.com>
2018-12-02 13:45:14 +0100

    cleanup & whitespace.

--- day2/challenge1.js
+++ day2/challenge1.js
@@ -1,45 +1,38 @@
 const fs = require("fs");
 
-String.prototype.count = function(c) {
-  let result = 0, i = 0;
-  for (i; i < this.length; i++) if (this[i] == c) result++;
-  return result;
-};
-
 fs.readFile("ids.txt", "utf8", function(err, contents) {
-  const ids = contents.split("\n");
-  let count2 = 0, count3 = 0;
+    let count2 = 0, count3 = 0;
+    const ids = contents.split("\n");
 
-  ids.forEach(id => {
-      const seen = new Map();
-      for (let i = 0; i < id.length; i++) {
-          const c = id.charAt(i);
-          if (seen.has(c)) {
+    ids.forEach(id => {
+        const seen = new Map();
+        for (let i = 0; i < id.length; i++) {
+            const c = id.charAt(i);
+            if (seen.has(c)) {
               seen.set(c, seen.get(c) + 1)
-          } else {
+            } else {
               seen.set(c, 1);
-          }
-      }
-      console.log(seen)
+            }
+        }
 
-      let found2 = false, found3 = false;
-      for (let c of seen.keys()) {
-         switch(seen.get(c)) {
-             case 2:
-                if (!found2) {
-                    found2 = true;
-                    count2++;
-                }
+        let found2 = false, found3 = false;
+        for (let c of seen.keys()) {
+            switch(seen.get(c)) {
+                case 2:
+                    if (!found2) {
+                        found2 = true;
+                        count2++;
+                    }
                 break;
-            case 3:
-                if(!found3) {
-                    found3 = true;
-                    count3++;
-                }
-                break;
-         }
-      }
-  });
+                case 3:
+                    if(!found3) {
+                        found3 = true;
+                        count3++;
+                    }
+                    break;
+            }
+        }
+    });
 
-  console.log ('amount 2: ', count2, ' amount 3: ', count3, ' solution: ', count2 * count3);
+    console.log ('amount 2: ', count2, ' amount 3: ', count3, ' solution: ', count2 * count3);
 });
