f57cf90074acb1d82d3f5cbedc3f23c0bb62a79f
Klaas-Jan Stellema <k.j.stellema@gmail.com>
2018-12-02 13:50:01 +0100

    removed map from iteratee

--- day2/challenge1.js
+++ day2/challenge1.js
@@ -1,18 +1,15 @@
 const fs = require("fs");
+const seen = new Map();
 
 fs.readFile("ids.txt", "utf8", function(err, contents) {
     let count2 = 0, count3 = 0;
     const ids = contents.split("\n");
 
     ids.forEach(id => {
-        const seen = new Map();
+        seen.clear();
         for (let i = 0; i < id.length; i++) {
             const c = id.charAt(i);
-            if (seen.has(c)) {
-              seen.set(c, seen.get(c) + 1)
-            } else {
-              seen.set(c, 1);
-            }
+            seen.set(c, seen.has(c) ? seen.get(c) + 1 : 1)
         }
 
         let found2 = false, found3 = false;
