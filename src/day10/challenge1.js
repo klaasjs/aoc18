f8e048308308cbd067095c7a0f87d64d3381c0b1
Klaas-Jan Stellema <klaas-jan@SchoolMaster.nl>
2018-12-19 16:49:15 +0100

    removed comments

--- src/day10/challenge1.js
+++ src/day10/challenge1.js
@@ -1,40 +1,6 @@
 const fs = require('fs'),
       assert = require('assert');
 
-/*const data = `position=< 9,  1> velocity=< 0,  2>
-position=< 7,  0> velocity=<-1,  0>
-position=< 3, -2> velocity=<-1,  1>
-position=< 6, 10> velocity=<-2, -1>
-position=< 2, -4> velocity=< 2,  2>
-position=<-6, 10> velocity=< 2, -2>
-position=< 1,  8> velocity=< 1, -1>
-position=< 1,  7> velocity=< 1,  0>
-position=<-3, 11> velocity=< 1, -2>
-position=< 7,  6> velocity=<-1, -1>
-position=<-2,  3> velocity=< 1,  0>
-position=<-4,  3> velocity=< 2,  0>
-position=<10, -3> velocity=<-1,  1>
-position=< 5, 11> velocity=< 1, -2>
-position=< 4,  7> velocity=< 0, -1>
-position=< 8, -2> velocity=< 0,  1>
-position=<15,  0> velocity=<-2,  0>
-position=< 1,  6> velocity=< 1,  0>
-position=< 8,  9> velocity=< 0, -1>
-position=< 3,  3> velocity=<-1,  1>
-position=< 0,  5> velocity=< 0, -1>
-position=<-2,  2> velocity=< 2,  0>
-position=< 5, -2> velocity=< 1,  2>
-position=< 1,  4> velocity=< 2,  1>
-position=<-2,  7> velocity=< 2, -2>
-position=< 3,  6> velocity=<-1, -1>
-position=< 5,  0> velocity=< 1,  0>
-position=<-6,  0> velocity=< 2,  0>
-position=< 5,  9> velocity=< 1, -2>
-position=<14,  7> velocity=<-2,  0>
-position=<-3,  6> velocity=< 2, -1>`;*/
-
-//const coordinates = data.split('\n').map(l => l.match(/-?\d+/g).map(Number));
-
 const coordinates = fs.readFileSync('input.txt').toString().split('\n').map(l => l.match(/-?\d+/g).map(Number));
 
 const closest = (timespan) => {
@@ -69,8 +35,6 @@ const closest = (timespan) => {
         if (minWidth > (minRight - minLeft)) {
             minWidth = minRight - minLeft;
         } else {
-            console.log('it seems its getting bigger: ', second, minWidth, minRight - minLeft);
-
             for (let t = 0; t < coordinates.length; t++) {
                 const coordinate2 = coordinates[t];
                 coordinate2[X] -= coordinate2[DX];
@@ -113,7 +77,5 @@ const plot = () => {
     }
 }
 plot();
-
-//console.log(coordinates);
 
 console.log('The coordinates were at ', second, ' the closest to each other.');
