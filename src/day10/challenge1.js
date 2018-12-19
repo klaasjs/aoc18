03a9fb2eaeb78396e42b2adfd1022f3bcb409e22
Klaas-Jan Stellema <klaas-jan@SchoolMaster.nl>
2018-12-19 16:47:24 +0100

    day10

--- src/day10/challenge1.js
+++ src/day10/challenge1.js
@@ -1,7 +1,7 @@
 const fs = require('fs'),
       assert = require('assert');
 
-const data = `position=< 9,  1> velocity=< 0,  2>
+/*const data = `position=< 9,  1> velocity=< 0,  2>
 position=< 7,  0> velocity=<-1,  0>
 position=< 3, -2> velocity=<-1,  1>
 position=< 6, 10> velocity=<-2, -1>
@@ -31,13 +31,65 @@ position=< 5,  0> velocity=< 1,  0>
 position=<-6,  0> velocity=< 2,  0>
 position=< 5,  9> velocity=< 1, -2>
 position=<14,  7> velocity=<-2,  0>
-position=<-3,  6> velocity=< 2, -1>`;
+position=<-3,  6> velocity=< 2, -1>`;*/
 
-const coordinates = data.split('\n').map(l => l.match(/-?\d+/g));
+//const coordinates = data.split('\n').map(l => l.match(/-?\d+/g).map(Number));
 
-const hasPosition = (x, y, offsetx, offsety) => {
+const coordinates = fs.readFileSync('input.txt').toString().split('\n').map(l => l.match(/-?\d+/g).map(Number));
+
+const closest = (timespan) => {
+    let minWidth = Number.MAX_VALUE, minHeight = Number.MAX_VALUE;
+    const X = 0, Y = 1, DX = 2, DY = 3;
+
+    for (let second = 0; second < timespan; second++) {
+        let minLeft = Number.MAX_VALUE, minTop = Number.MAX_VALUE, minRight = 0, minBottom = 0;
+
+        for (let c = 0; c < coordinates.length; c++) {
+            const coordinate = coordinates[c];
+            coordinate[X] += coordinate[DX];
+            coordinate[Y] += coordinate[DY];
+
+            if (coordinate[X] < minLeft) {
+                minLeft = coordinate[X];
+            }
+
+            if (coordinate[Y] < minTop) {
+                minTop = coordinate[Y];
+            }
+
+            if (coordinate[X] >= minRight) {
+                minRight = coordinate[X];
+            }
+
+            if (coordinate[Y] >= minBottom) {
+                minBottom = coordinate[Y];
+            }
+        }
+
+        if (minWidth > (minRight - minLeft)) {
+            minWidth = minRight - minLeft;
+        } else {
+            console.log('it seems its getting bigger: ', second, minWidth, minRight - minLeft);
+
+            for (let t = 0; t < coordinates.length; t++) {
+                const coordinate2 = coordinates[t];
+                coordinate2[X] -= coordinate2[DX];
+                coordinate2[Y] -= coordinate2[DY];
+            }
+
+            return second;
+        }
+    }
+
+    return -1;
+}
+
+const second = closest(12000);
+
+const hasPointAt = (x,y) => {
     for (let c = 0; c < coordinates.length; c++) {
-        if (coordinates[c][0] + offsetx === x && coordinate[c][1] + offsety === y) {
+        const coordinate = coordinates[c];
+        if (coordinate[0]-100 === x && coordinate[1]-100 === y) {
             return true;
         } 
     }
@@ -45,17 +97,23 @@ const hasPosition = (x, y, offsetx, offsety) => {
     return false;
 }
 
+
 const plot = () => {
-    for (let y = 0; y < 20; y++) {
+    for (let y = 0; y < 90; y++) {
         const line = [];
-        for (let x = 0; x < 30; x++) {
-            if (hasPosition(x,y, 0, 0)) {
-                line.push('#');
+        for (let x = 0; x < 170; x++) {
+            if (hasPointAt(x,y)) {
+                line.push('#')
             } else {
                 line.push('.');
             }
         }
+
         console.log(line.join(''));
     }
 }
 plot();
+
+//console.log(coordinates);
+
+console.log('The coordinates were at ', second, ' the closest to each other.');
