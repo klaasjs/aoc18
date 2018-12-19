1946f97fa22b167521fadf308420562f331cf2aa
Klaas-Jan Stellema <klaas-jan@SchoolMaster.nl>
2018-12-19 17:22:35 +0100

    simple optimization

--- src/day11/challenge1.js
+++ src/day11/challenge1.js
@@ -1,15 +1,8 @@
 const assert = require('assert');
 
 const power = (x, y, sn) => {
-    const rackId = x + 10;
-    const powerLevel = rackId * y;
-    const withSerial = powerLevel + sn;
-    const withRackId = withSerial * rackId;
-
-    const hundred = withRackId.toString().charAt(withRackId.toString().length - 3);
-
-    const result = parseInt(hundred) - 5;
-    return result;
+    const result = (((x + 10) * y) + sn * (x + 10)).toString();
+    return parseInt(result.charAt(result.length - 3)) - 5;
 }
 
 assert.equal(power(3, 5, 8), 4);
